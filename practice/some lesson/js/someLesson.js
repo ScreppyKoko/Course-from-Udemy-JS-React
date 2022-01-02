cc509240d85de142fee36bb4f9188c77e72d55a7
Alex Chernyshov <kozel95@bk.ru>
2022-01-02 17:06:30 +0300

    some changes

--- practice/some lesson/js/someLesson.js
+++ practice/some lesson/js/someLesson.js
@@ -2,20 +2,20 @@
 
 //* task_1
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
-    a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = +prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = +prompt('На сколько оцените его?', '');
+   a = prompt('Один из последних просмотренных фильмов?', ''),
+   b = +prompt('На сколько оцените его?', ''),
+   c = prompt('Один из последних просмотренных фильмов?', ''),
+   d = +prompt('На сколько оцените его?', '');
 
-    console.log(numberOfFilms);
+console.log(numberOfFilms);
 
 //* task_2 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: {},
-    privat: false,
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: {},
+   privat: false,
 };
 console.log(personalMovieDB);
 
@@ -23,3 +23,9 @@ console.log(personalMovieDB);
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 console.log(personalMovieDB.movies);
+
+const num = 50;
+switch (num) {
+   case 51: console.log('Не верно');
+      break;
+}
