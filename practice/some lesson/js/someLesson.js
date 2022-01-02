2bc137ebc977a2911a0f89ab96034464e02b2c15
Skreppy Coco <kozel95@bk.ru>
2022-01-03 02:51:06 +0300

    practice №3 completed

--- practice/some lesson/js/someLesson.js
+++ practice/some lesson/js/someLesson.js
@@ -3,14 +3,17 @@
 //! Practice #1 начинаем создавать приложение! (работа с объектами и запросы к пользователю)
 //todo TASK_1 Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 //* 'Сколько фильмов вы уже посмотрели?'
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//     b = +prompt('На сколько оцените его?', ''),
-//     c = prompt('Один из последних просмотренных фильмов?', ''),
-//     d = +prompt('На сколько оцените его?', '');
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    console.log(numberOfFilms);
 
-console.log(numberOfFilms);
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+// start();
 
 //todo TASK_2 Создать объект personalMovieDB и в него поместить такие свойства:
 //* - count - сюда передается ответ на первый вопрос
@@ -26,8 +29,6 @@ const personalMovieDB = {
     genres: [],
     privat: false,
 };
-
-console.log(personalMovieDB);
 
 //todo TASK_3 Задайте пользователю по два раза вопросы:
 //*     - 'Один из последних просмотренных фильмов?'
@@ -45,37 +46,44 @@ console.log(personalMovieDB);
 //! Practice #2 (Применяем условия и циклы )
 
 //todo TASK_1. Автоматизировать вопросы пользователю про фильмы при помощи цикла
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', '');
-    const b = +prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', '');
+        const b = +prompt('На сколько оцените его?', '');
 
-    //todo TASK_2. Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-    //* отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-    //* возвращаем пользователя к вопросам опять
-    if (a != null && b != null & a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+        //todo TASK_2. Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+        //* отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+        //* возвращаем пользователя к вопросам опять
+        if (a != null && b != null & a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
-console.log(personalMovieDB);
+
+// rememberMyFilms();
 
 //todo TASK_3. При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
 //* "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
 //* "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    }
+    else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    }
+    else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман!');
+    } else {
+        console.log('Произошла ошибка!');
+    }
 }
-else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-}
-else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман!');
-} else {
-    console.log('Произошла ошибка!');
-}
+// detectPersonalLevel();
 
 //todo TASK_4. Потренироваться и переписать цикл еще двумя способами
 //* (переписать циклы с помощью while / do while)
@@ -112,3 +120,29 @@ else if (personalMovieDB.count >= 30) {
 // console.log(personalMovieDB);
 
 //! Practice #3 ( Используем функции )
+
+//todo TASK_1. Первую часть задания повторить по уроку
+//* вывел в отдельные функции условия, цикл.
+
+//todo TASK_2. Создать функцию showMyDB, которая будет проверять свойство privat.
+// Если стоит в позиции false - выводит в консоль главный объект программы
+function showMyDB(hidden) { //* мое решение было без аргумента =(
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+//todo TASK_3.  Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос:
+// "Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных genres
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+        //* мое решение(не очень):
+        // const checkLikedGenres = prompt(`Ваш любимый жанр под номером ${i}`);
+        // personalMovieDB.genres.push(checkLikedGenres);
+    }
+}
+writeYourGenres();
+
+//? P.S. Функции вызывать не обязательно!!! 
