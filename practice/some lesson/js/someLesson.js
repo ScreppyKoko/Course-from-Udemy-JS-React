a67b782b69093389074e97dd6e77dc5e2b4d3d2f
Skreppy Coco <kozel95@bk.ru>
2022-01-02 16:13:47 +0300

    Completed Task №1

--- practice/some lesson/js/someLesson.js
+++ practice/some lesson/js/someLesson.js
@@ -1,2 +1,25 @@
 'use strict';
 
+//* task_1
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+    a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = +prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = +prompt('На сколько оцените его?', '');
+
+    console.log(numberOfFilms);
+
+//* task_2 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false,
+};
+console.log(personalMovieDB);
+
+//* task_3
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB.movies);
