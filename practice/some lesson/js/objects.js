df03570ea63dea18e0e748d3c5e0988a67d6754c
Alex Chernyshov <kozel95@bk.ru>
2022-01-08 23:45:19 +0300

    some changes

--- practice/some lesson/js/objects.js
+++ practice/some lesson/js/objects.js
@@ -94,7 +94,7 @@ let menu = {
 function multiplyNumeric(obj) {
     for (let key in obj) {
         if (typeof obj[key] == "number") {
-            obj[key] *= 2; 
+            obj[key] *= 2;
             console.log(obj[key]);
         }
     }
--- practice/some lesson/js/script.js
+++ practice/some lesson/js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+
+(function () {
+   var name = 2 * 2;
+   console.log(name);
+   return name;
+})();
