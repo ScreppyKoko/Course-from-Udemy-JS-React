b167229f967714badb35c958662cdf6430bbf501
Alex Chernyshov <kozel95@bk.ru>
2022-01-22 03:16:21 +0300

    bug in forEach fixed, payables.js

--- practice/some lesson/js/payables.js
+++ practice/some lesson/js/payables.js
@@ -6,33 +6,38 @@ const payablesSum = document.querySelector('.payables__sum-input');
 const payablesSumTotal = document.querySelector('.payables__result-sum');
 const payablesList = document.querySelector('.payables__list');
 
-
 const payablesObj = {
    date: ['2022-12-30', '2022-01-11', '2022-01-13', '2022-01-21'],
    sum: [3045, 656, 666, 7863]
 };
+
+let newObj = Object.assign(...payablesObj.date.map((n, i) => ({ [n]: payablesObj.sum[i] })));
+let newObj2 = Object.entries(newObj);
+console.log(newObj);
+
 
 //todo ****** сортировка при желании
 const sortArr = (arr) => {
    arr.sort();
 };
 
-let summative = 0;
 function getPlus() {
+   let summative = 0;
    for (let i = 0; i < payablesObj.sum.length; i++) {
       summative += payablesObj.sum[i];
    }
+   payablesSumTotal.textContent = summative + ' рублей';
 }
 
 function createPayablesList() {
    payablesList.innerHTML = '';
-   getPlus();
-   payablesObj.sum.forEach(sum => {
+   payablesObj.date.forEach(date => {
       payablesList.innerHTML += `
-      <li class="payables__list-item">${sum}</li>
+      <li class="payables__list-item">${Object.entries(newObj)}</li>
       `;
    });
-   payablesSumTotal.textContent = summative + ' рублей';
+   getPlus();
+   sortArr(payablesObj.date);
 }
 createPayablesList();
 
@@ -44,8 +49,5 @@ payablesForm.addEventListener('submit', (event) => {
 
    payablesObj.date.push(newPayDate);
    payablesObj.sum.push(+newPaySum);
-   console.log(payablesObj);
-   getPlus();
    createPayablesList();
 });
-
